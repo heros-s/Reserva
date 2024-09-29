a352265b418559e18a956f89863fff894ba53265
Heros Dinão <herosdefreitas@gmail.com>
2024-09-29 00:06:57 -0300

    Preparando DB

---
+++ APIReserva/Models/AppDBContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace APIReserva.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Sala> Salas { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- APIReserva/Program.cs
+++ APIReserva/Program.cs
@@ -2,9 +2,22 @@
 
 using APIReserva.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar o banco de dados SQLite
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// Aplicar migrações automaticamente
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 List<Sala> salas = new List<Sala>
         {
